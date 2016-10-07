9ac26e4edc793411029013af2116b5d98d0a1283
jroberts97 <jroberts@utcmediacityuk.org.uk>
2016-10-08 00:58:06 +0100

    Add files via upload

---
+++ MinimumOfThree.java
@@ -0,0 +1,58 @@
+package minimumofthree;
+// @author JRoberts
+
+public class MinimumOfThree {
+    
+    public static void main(String[] args) {
+       int a [][] = {
+           {7, 15, 300}  
+        };
+       
+       int b [][] = {
+           {3, 20, 550}
+       };
+       
+       int c [][] = {
+           {5, 40, 137}
+       };
+       
+       int x = 0;
+       int y = 0;
+       int first;
+       int second;
+       int third;
+       
+       if (a[y][x] < b[y][x] && a[y][x] < c[y][x]){
+           first = a[y][x]; 
+           }
+       else if (b[y][x] < c[y][x]){
+           first = b[y][x];
+       }
+       else{
+           first = c[y][x];
+           }
+       
+       if (a[y][x+1] < b[y][x+1] && a[y][x+1] < c[y][x+1]){
+           second = a[y][x+1]; 
+           }
+       else if (b[y][x+1] < c[y][x+1]){
+           second = b[y][x+1];
+       }
+       else{
+           second = c[y][x+1];
+           }
+       
+        if (a[y][x+2] < b[y][x+2] && a[y][x+2] < c[y][x+2]){
+            third = a[y][x+2]; 
+           }
+        else if (b[y][x+2] < c[y][x+2]){
+            third = b[y][x+2];
+        }
+       else{
+           third = c[y][x+2];
+           }
+        
+        System.out.println("Answer: " + first + ", " + second + ", " + third);
+    }
+    
+}
