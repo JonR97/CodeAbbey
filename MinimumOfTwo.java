59e174ba537f7562641ff7875e582dbf3d569864
jroberts97 <jroberts@utcmediacityuk.org.uk>
2016-10-07 17:21:59 +0100

    Add files via upload

---
+++ MinimumOfTwo.java
@@ -0,0 +1,45 @@
+package minimumoftwo;
+// @author JRoberts
+ 
+public class MinimumOfTwo {
+
+    public static void main(String[] args) {
+       int a [][] = {
+           {5, 2, 100}  
+        };
+       
+       int b [][] = {
+           {3, 8, 15}
+       };
+       int x = 0;
+       int y = 0;
+       int first;
+       int second;
+       int third;
+       
+       if (a[y][x] < b[y][x]){
+           first = a[y][x]; 
+           }
+       else{
+           first = b[y][x];
+           }
+       
+       if (a[y][x+1] < b[y][x+1]){
+           second = a[y][x+1]; 
+           }
+       else{
+           second = b[y][x+1];
+           }
+       
+        if (a[y][x+2] < b[y][x+2]){
+           third = a[y][x+2]; 
+           }
+       else{
+           third = b[y][x+2];
+           }
+        
+        System.out.println("Answer: " + first + ", " + second + ", " + third);
+       
+    }
+    
+}
